886032992e9c6e2f8f59bb919a97beb611d1b4e7
iamyeti <synk.pit@gmail.com>
2015-04-05 01:59:35 +0300

    tags route, tag page, 404 page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pymongo
 from pymongo import MongoClient
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
@@ -13,6 +13,13 @@ objects = db['objects']
 @app.route("/")
 def hello():
     return render_template('index.html', show_tags=[tag['name'] for tag in tags.find()], show_objects=[obj['name'] for obj in objects.find()])
+
+@app.route('/<tag>')
+def tag_page(tag):
+    tags_list = [i_tag['name'] for i_tag in tags.find()]
+    if tag not in tags_list:
+    	return render_template('404.html', show_name=tag)
+    return render_template('tag.html', show_name=tag)
 
 if __name__ == "__main__":
     app.run()
