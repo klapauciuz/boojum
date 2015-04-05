55dbf8efb4a294258e3051b46d6489b75b309ef3
Yury Shapkarin <yury@shapkarin.me>
2015-04-05 10:32:06 +0300

    mess api

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pymongo
 from pymongo import MongoClient
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, json, jsonify
+import json
+from bson import json_util
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
@@ -16,17 +19,36 @@ def hello():
 
 @app.route('/<tag>')
 def tag_page(tag):
-    tags_list = [i_tag['name'] for i_tag in tags.find()]
-    if tag not in tags_list:
-    	return render_template('404.html', show_name=tag)
-    return render_template('tag.html', show_name=tag)
+    tag = tags.find_one({'name': tag})
+    if tag:
+        return render_template('tag.html', show_name=tag['name'])
+    else:
+        return render_template('404.html', show_name=tag)
 
-@app.route('/api/<test>')
-def tags_f():
-#    app.logger.debug('name is %d', test)
-    response.status_code(500)
-    response.data(test)
-    return 'test %d' % test
+@app.route('/api/tag/<id>')
+def get_tag(id):
+    return jsonify(
+        name=id
+    )
+def output_json(obj, code, headers=None):
+    """
+    This is needed because we need to use a custom JSON converter
+    that knows how to translate MongoDB types to JSON.
+    """
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+
+    return resp
+
+@app.route('/api/tags')
+def tags_list():
+    if request.method == 'GET':
+#        limit = int(request.args.get('limit', 10))
+#        offset = int(request.args.get('offset', 0))
+        json_results = []
+        json_docs = [json.dumps(doc, default=json_util.default) for doc in tags.find()]
+        return str(json_docs)
+
 
 if __name__ == "__main__":
     app.run()
