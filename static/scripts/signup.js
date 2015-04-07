77ce1efed25c17513dfc792e9faad1312c7e3f2c
iamyeti <synk.pit@gmail.com>
2015-04-07 13:20:41 +0300

    cockeyed sign up

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-  $('form#add').submit(function(event) {
+  $('form#signup').submit(function(event) {
     event.preventDefault();
     var self = $(this),
     postData = self.serializeArray();
 
     $.ajax({
-        url : '/api/add/tag',
+        url : '/api/signup',
         type: 'POST',
         data : postData,
         success:function(response) {
-            window.location.replace("/");
+            console.log(postData)
+            $('form#signup').replaceWith("<h2>welcome, "+postData[0].value+"!</h2>")
+
         },
         error: function(err) {
           console.log(JSON.stringify(err));
